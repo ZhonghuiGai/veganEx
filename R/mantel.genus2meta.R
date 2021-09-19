218fe93ca877574397f0cc6bde5537bf3c00b71f
ubuntu <ubuntu@localhost.localdomain>
2021-09-19 11:15:53 +0800

    version 0.0.1

--- R/mantel.genus2meta.R
+++ R/mantel.genus2meta.R
@@ -1,39 +1,40 @@
-#' Title
+#' Test the correlation of two matrix using the mantel function in the vegan package
 #'
-#' @param genusdata
-#' @param metadata
-#' @param method
+#' @param genusdata the OTU table
+#' @param metadata the metadata
+#' @param method method to be used for correlation analysis, one of pearson and spearman, the default value is spearman
 #'
-#' @return
+#' @return a ggplot object
 #' @export
 #'
+#' @author Zhonghui Gai
 #' @examples
+#' mantel.genus2meta(genusdata, metadata)
 mantel.genus2meta <- function(genusdata, metadata,
                           method = "pearson"){
-  genus.dist <- vegan::vegdist(genusdata, method = "bray")#根据物种丰度数据，计算样方的bray距离
-  #根据样方临床指标，计算欧几里德距离，为了消除量纲影响，先进行标准化
-  metadata <- base::scale(metadata, center = TRUE, scale = TRUE)#标准化
-  meta.dist <- vegan::vegdist(metadata, method = "euclid")#计算欧几里德距离
+  genus.dist <- vegan::vegdist(genusdata, method = "bray")
+  metadata <- base::scale(metadata, center = TRUE, scale = TRUE)
+  meta.dist <- vegan::vegdist(metadata, method = "euclid")
   mantel.result <- vegan::mantel(genus.dist, meta.dist, permutations=9999, method = method)
-  # 构建画图用的数据框
+  # construct the data frame for ggplot2
   data <- data.frame(x = as.vector(genus.dist), y = as.vector(meta.dist))
-  # 作图
-  p <- ggplot2::ggplot(data = data, aes(x = x, y = y))+
-    geom_point(size=1, alpha=1, color="gray10")+
+  # make the plot
+  p <- ggplot2::ggplot(data = data, aes(x = x, y = y)) +
+    geom_point(size = 1, alpha =1 , color = "gray10")+
     geom_smooth(method = lm, colour = "red", alpha = 0.2, se = TRUE)+
     labs(x = "Bray-Curtis dissimilarity", y = "metadata Euclidean dissimilarity") +
-    theme( axis.text.x = element_text(face = "bold",colour = "black", size = 12),
-           axis.text.y = element_text(face = "bold", size = 11, colour = "black"),
-           axis.title= element_text(face = "bold", size = 14, colour = "black"),
-           panel.background = element_blank(),
-           panel.border = element_rect(fill = NA, colour = "black"),
-           legend.position = "top",
-           legend.text = element_text(size = 10, face = "bold"),
-           legend.title = element_text(size = 11, face = "bold"))
-  # 提取统计量
+    theme(axis.text.x = element_text(face = "bold",colour = "black", size = 12),
+          axis.text.y = element_text(face = "bold", size = 11, colour = "black"),
+          axis.title= element_text(face = "bold", size = 14, colour = "black"),
+          panel.background = element_blank(),
+          panel.border = element_rect(fill = NA, colour = "black"),
+          legend.position = "top",
+          legend.text = element_text(size = 10, face = "bold"),
+          legend.title = element_text(size = 11, face = "bold"))
+  # extract the statistics
   r <- mantel.result$statistic
   p.val <- mantel.result$signif
-  # 添加文本
+  # add the text
   p <- p + ggplot2::annotate(geom = "text", x = mean(data$x), y = max(data$y)*1.05,
                              label = paste0("Mantel statistic r: ", round(r, 3)),
                              size = 5, fontface = "bold")
